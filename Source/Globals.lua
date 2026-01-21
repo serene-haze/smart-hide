81676f809c66b00c1c955b42e487d0c287268723
Author <none>
2026-01-21 23:17:43 +0000

    Default target mode to off

--- Source/Globals.lua
+++ Source/Globals.lua
@@ -5,7 +5,7 @@ addon.defaults = {
         ["interactive"] = false,
         ["health"] = 100,
         --["power"] = false,
-        ["target"] = true,
+        ["target"] = false,
         ["group"] = false,
         ["ctrl"] = false,
         ["mouseover"] = false
@@ -18,7 +18,7 @@ Player Frame Smart Hide commands:
 |cffeab517/smarthide settings|r - Displays the currently set option values
 |cffeab517/smarthide interactive on|off|r |cffadadad(default: off)|r - Toggle player frame interactivity when hidden.
 |cffeab517/smarthide health 1-100|off|r |cffadadad(default: 100)|r - The player health % below which the player frame will be shown.
-|cffeab517/smarthide target on|off|r |cffadadad(default: on)|r - Show the player frame when the player has a target.
+|cffeab517/smarthide target on|off|r |cffadadad(default: off)|r - Show the player frame when the player has a target.
 |cffeab517/smarthide group on|off|r |cffadadad(default: off)|r - Show the player frame when in a group.
 |cffeab517/smarthide ctrl on|off|r |cffadadad(default: off)|r - Show the player frame when ctrl is held down.
 |cffeab517/smarthide mouseover on|off|r |cffadadad(default: off)|r - Show the player frame on mouseover.]];
